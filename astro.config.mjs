90027adb53e8ca091b4e8b8bc4179d918768efab
Gregory Heitz <324670+edno@users.noreply.github.com>
2026-08-29 17:19:47 +0200

    fix sidebar config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,30 +1,34 @@
 // @ts-nocheck
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 import catppuccin from "@catppuccin/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	base: '/demo-astro-starlight',
-	integrations: [
-		starlight({
-			plugins: [catppuccin()],
-			credits: true,
-			lastUpdated: true,
-			title: 'GraphQL-Markdown demo',
-			social: [
-				{ icon: 'github', label: 'GitHub', href: 'https://github.com/graphql-markdown/graphql-markdown' },
-			],
-			sidebar: [
-				{
-					label: 'Operations',
-					autogenerate: { directory: 'operations' },
-				},
-				{
-					label: 'Types',
-					autogenerate: { directory: 'types' },
-				},
-			],
-		}),
-	],
+  base: "/demo-astro-starlight",
+  integrations: [
+    starlight({
+      plugins: [catppuccin()],
+      credits: true,
+      lastUpdated: true,
+      title: "GraphQL-Markdown demo",
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/graphql-markdown/graphql-markdown",
+        },
+      ],
+      sidebar: [
+        {
+          label: "Operations",
+          items: [{ autogenerate: { directory: "operations" } }],
+        },
+        {
+          label: "Types",
+          items: [{ autogenerate: { directory: "types" } }],
+        },
+      ],
+    }),
+  ],
 });
