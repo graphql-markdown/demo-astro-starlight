9dcd831e34c650a4b14c61fd44e27bb05ae9ff17
Gregory Heitz <324670+edno@users.noreply.github.com>
2026-08-29 17:14:21 +0200

    remove unmaintained starlight-site-graph

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,13 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import catppuccin from "@catppuccin/starlight";
-import starlightSiteGraph from 'starlight-site-graph';
 
 // https://astro.build/config
 export default defineConfig({
 	base: '/demo-astro-starlight',
 	integrations: [
 		starlight({
-			plugins: [catppuccin(), starlightSiteGraph()],
+			plugins: [catppuccin()],
 			credits: true,
 			lastUpdated: true,
 			title: 'GraphQL-Markdown demo',
